2d6c77a39dc51075ed1997a25a17650d9e2942cf
Federico Pizzuti <pizzutifederico27@gmail.com>
2015-06-05 12:03:31 +0100

    Added basic gdt, idt and isr functionality

---
+++ current/src/isr.cpp
@@ -0,0 +1,166 @@
+#include <system.h>
+#include <isr.h>
+#include <idt.h>
+#include <scrn.h>
+
+extern "C" void _isr0();
+extern "C" void _isr1();
+extern "C" void _isr2();
+extern "C" void _isr3();
+extern "C" void _isr4();
+extern "C" void _isr5();
+extern "C" void _isr6();
+extern "C" void _isr7();
+extern "C" void _isr8();
+extern "C" void _isr9();
+extern "C" void _isr10();
+extern "C" void _isr11();
+extern "C" void _isr12();
+extern "C" void _isr13();
+extern "C" void _isr14();
+extern "C" void _isr15();
+extern "C" void _isr16();
+extern "C" void _isr17();
+extern "C" void _isr18();
+extern "C" void _isr19();
+extern "C" void _isr20();
+extern "C" void _isr21();
+extern "C" void _isr22();
+extern "C" void _isr23();
+extern "C" void _isr24();
+extern "C" void _isr25();
+extern "C" void _isr26();
+extern "C" void _isr27();
+extern "C" void _isr28();
+extern "C" void _isr29();
+extern "C" void _isr30();
+extern "C" void _isr31();
+
+exception_handler handlers[EXCEPTION_HANDLER_COUNT];
+
+void set_handler(int n, exception_handler h) {
+	handlers[n] = h;
+}
+
+void clear_handler(int n) {
+	handlers[n] = 0;
+}
+
+void setup_hanlders() {
+	for(int i = 0; i < EXCEPTION_HANDLER_COUNT;i++) {
+		handlers[i] = 0;
+	}
+}
+
+/* This is a very repetitive function... it's not hard, it's
+*  just annoying. As you can see, we set the first 32 entries
+*  in the IDT to the first 32 ISRs. We can't use a for loop
+*  for this, because there is no way to get the function names
+*  that correspond to that given entry. We set the access
+*  flags to 0x8E. This means that the entry is present, is
+*  running in ring 0 (kernel level), and has the lower 5 bits
+*  set to the required '14', which is represented by 'E' in
+*  hex. */
+void isr_install()
+{
+    idt_set_gate(0, (unsigned)_isr0, 0x08, 0x8E);
+    idt_set_gate(1, (unsigned)_isr1, 0x08, 0x8E);
+    idt_set_gate(2, (unsigned)_isr2, 0x08, 0x8E);
+    idt_set_gate(3, (unsigned)_isr3, 0x08, 0x8E);
+    idt_set_gate(4, (unsigned)_isr4, 0x08, 0x8E);
+    idt_set_gate(5, (unsigned)_isr5, 0x08, 0x8E);
+    idt_set_gate(6, (unsigned)_isr6, 0x08, 0x8E);
+    idt_set_gate(7, (unsigned)_isr7, 0x08, 0x8E);
+                              
+    idt_set_gate(8, (unsigned)_isr8, 0x08, 0x8E);
+    idt_set_gate(9, (unsigned)_isr9, 0x08, 0x8E);
+    idt_set_gate(10, (unsigned)_isr10, 0x08, 0x8E);
+    idt_set_gate(11, (unsigned)_isr11, 0x08, 0x8E);
+    idt_set_gate(12, (unsigned)_isr12, 0x08, 0x8E);
+    idt_set_gate(13, (unsigned)_isr13, 0x08, 0x8E);
+    idt_set_gate(14, (unsigned)_isr14, 0x08, 0x8E);
+    idt_set_gate(15, (unsigned)_isr15, 0x08, 0x8E);
+                               
+    idt_set_gate(16, (unsigned)_isr16, 0x08, 0x8E);
+    idt_set_gate(17, (unsigned)_isr17, 0x08, 0x8E);
+    idt_set_gate(18, (unsigned)_isr18, 0x08, 0x8E);
+    idt_set_gate(19, (unsigned)_isr19, 0x08, 0x8E);
+    idt_set_gate(20, (unsigned)_isr20, 0x08, 0x8E);
+    idt_set_gate(21, (unsigned)_isr21, 0x08, 0x8E);
+    idt_set_gate(22, (unsigned)_isr22, 0x08, 0x8E);
+    idt_set_gate(23, (unsigned)_isr23, 0x08, 0x8E);
+                               
+    idt_set_gate(24, (unsigned)_isr24, 0x08, 0x8E);
+    idt_set_gate(25, (unsigned)_isr25, 0x08, 0x8E);
+    idt_set_gate(26, (unsigned)_isr26, 0x08, 0x8E);
+    idt_set_gate(27, (unsigned)_isr27, 0x08, 0x8E);
+    idt_set_gate(28, (unsigned)_isr28, 0x08, 0x8E);
+    idt_set_gate(29, (unsigned)_isr29, 0x08, 0x8E);
+    idt_set_gate(30, (unsigned)_isr30, 0x08, 0x8E);
+    idt_set_gate(31, (unsigned)_isr31, 0x08, 0x8E);
+}
+
+/* This is a simple string array. It contains the message that
+*  corresponds to each and every exception. We get the correct
+*  message by accessing like:
+*  exception_message[interrupt_number] */ 
+char *exception_messages[] =
+{
+    "Division By Zero",
+    "Debug",
+    "Non Maskable Interrupt",
+    "Breakpoint",
+    "Into Detected Overflow",
+    "Out of Bounds",
+    "Invalid Opcode",
+    "No Coprocessor",
+
+    "Double Fault",
+    "Coprocessor Segment Overrun",
+    "Bad TSS",
+    "Segment Not Present",
+    "Stack Fault",
+    "General Protection Fault",
+    "Page Fault",
+    "Unknown Interrupt",
+
+    "Coprocessor Fault",
+    "Alignment Check",
+    "Machine Check",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved"
+};
+
+/* All of our Exception handling Interrupt Service Routines will
+*  point to this function. This will tell us what exception has
+*  happened! Right now, we simply halt the system by hitting an
+*  endless loop. All ISRs disable interrupts while they are being
+*  serviced as a 'locking' mechanism to prevent an IRQ from
+*  happening and messing up kernel data structures */
+extern "C" void _fault_handler(struct regs *r)
+{
+	
+    if (r->int_no < 32)
+    {
+		exception_handler h = handlers[r->int_no];
+		if(h != 0) {
+			h(r);
+		} else {
+			puts(exception_messages[r->int_no]);
+			puts("Exception. System Halted!\n");
+			for (;;);
+		}
+    }
+}
--- current/src/kernel.cpp
+++ current/src/kernel.cpp
@@ -1,10 +1,17 @@
 #include <system.h>
 #include <scrn.h> 
+#include <gdt.h>
+#include <idt.h>
+#include <isr.h>
+
 #if defined(__cplusplus)
 extern "C" /* Use C linkage for kernel_main. */
 #endif
 void kernel_main() {
 	init_video();
+	gdt_install();
+	idt_install();
+	isr_install();
 	cls();
-	puts("Welcome to PizOS 0.0000000.....000001");
+	puts("Welcome to PizOS 0.0000000.....000001\n");
 }
